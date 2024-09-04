24cf536eb5f6c29aa0c4220db7d54c93206cee8b
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-09-04 00:34:50 -0500

    tarea

--- src/classes/Hero.ts
+++ src/classes/Hero.ts
@@ -1,12 +1,15 @@
 import { NumericLiteral } from "typescript";
+import powers from "../data/powers";
 
 export class Hero {
     constructor (
         public name: string,
         public powerId: number,
         public age: number,
-    ){
+    ){}
 
+    get power():string{
+        return powers.find(power => power.id === this.powerId)?.desc || 'not found';
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
-// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
-import * as HeroClasses from "./classes/Hero";
-import powers,{Power} from "./data/powers";
-import { Power } from './data/powers';
+import { Hero} from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import powers,{Power} from "./data/powers";
+// import { Power } from './data/powers';
 
 
 console.log('Hola Mundo!!!!');
 
 // const ironman = new SuperHero('yuri', 1500,38);
-const ironman = new HeroClasses.Hero('yuri', 1500,38);
+const ironman = new Hero('yuri', 1,38);
 console.log(ironman);
-
-console.log(powers)
-
-
-
+console.log(ironman.power );
 
