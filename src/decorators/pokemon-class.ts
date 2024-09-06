c4a3448cb5433d41b537b60311ca32952881957b
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-09-06 11:08:40 -0500

    factory decorators

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -2,7 +2,17 @@ function printToConsole( constructor: Function ){
     console.log( constructor );
 }
 
-@printToConsole
+const printToConsoleConditional = ( print:boolean = false ): Function => {
+    // return () => console.log('Hola Mundo'); 
+    if( print ){
+        return printToConsole;
+    }else{
+        return() => {}
+    }
+
+}
+
+@printToConsoleConditional( false )
 export class Pokemon {
 
      public publicApi: string = 'https://pokeapi.co';
