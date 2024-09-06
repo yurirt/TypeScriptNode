012951460b95d6abb4ff62c2cbd921785e9af717
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-09-05 23:14:51 -0500

    generic example

---
+++ src/generics/get-pokemon.ts
@@ -0,0 +1,11 @@
+
+
+import axios from "axios";
+
+
+export const getPokemon = async( pokemonId: number ) => {
+
+    const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    console.log(resp);
+    return 1;
+}
